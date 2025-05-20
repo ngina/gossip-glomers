54497d0e334428b945ff001fc46ba41f9914de48
Ngina <ngina@users.noreply.github.com>
2025-05-20 02:08:35 -0700

    refactor: make server variable private, refactor id into server struct

--- challenge-2-unique-id/main.go
+++ challenge-2-unique-id/main.go
@@ -8,9 +8,14 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+type server struct {
+    n *maelstrom.Node
+    id int
+}
+
 func main() {
 	n := maelstrom.NewNode()
-    s := &Server{n: n}
+    s := &server{n: n}
     n.Handle("generate", s.generate)
 
     if err := n.Run(); err != nil {
@@ -18,17 +23,12 @@ func main() {
 	}
 }
 
-var id = 0
-type Server struct {
-    n *maelstrom.Node
+func (s *server) generate_unique_id() string {
+	s.id = s.id + 1
+	return fmt.Sprintf("%v%v", s.n.ID(), s.id)
 }
 
-func (s *Server) generate_unique_id() string {
-	id = id + 1
-	return fmt.Sprintf("%v%v", s.n.ID(), id)
-}
-
-func (s *Server) generate(msg maelstrom.Message) error {
+func (s *server) generate(msg maelstrom.Message) error {
     var body map[string]any
     if err := json.Unmarshal(msg.Body, &body); err != nil {
         return err
